3ee29c618336d16837ccd0d8342b499f8b5cc9ed
Simon Pham <simonpham.dn@gmail.com>
2023-10-16 23:54:32 +0700

    💄 Use outline icon for file entities

--- core_packages/core_ui/lib/extensions/file.dart
+++ core_packages/core_ui/lib/extensions/file.dart
@@ -7,30 +7,30 @@ extension FileExtension on File {
     final type = kMimeTypes[fileType.mimeType];
     switch (type) {
       case FileType.pdf:
-        return Assets.icons.filesAndFolder.solid.filePdf;
+        return Assets.icons.filesAndFolder.outline.filePdf;
       case FileType.doc:
       case FileType.docx:
-        return Assets.icons.filesAndFolder.solid.fileDoc;
+        return Assets.icons.filesAndFolder.outline.fileDoc;
       case FileType.jpeg:
-        return Assets.icons.filesAndFolder.solid.fileJpg;
+        return Assets.icons.filesAndFolder.outline.fileJpg;
       case FileType.png:
-        return Assets.icons.filesAndFolder.solid.filePng;
+        return Assets.icons.filesAndFolder.outline.filePng;
       default:
     }
 
     switch (fileType.contentType) {
       case ContentType.audio:
-        return Assets.icons.filesAndFolder.solid.fileMusic;
+        return Assets.icons.filesAndFolder.outline.fileMusic;
       case ContentType.image:
-        return Assets.icons.multimediaAndAudio.solid.image;
+        return Assets.icons.multimediaAndAudio.outline.image;
       case ContentType.video:
-        return Assets.icons.filesAndFolder.solid.fileVideo;
+        return Assets.icons.filesAndFolder.outline.fileVideo;
       case ContentType.text:
-        return Assets.icons.editor.solid.documentText;
+        return Assets.icons.editor.outline.documentText;
       case ContentType.application:
-        return Assets.icons.filesAndFolder.solid.archive;
+        return Assets.icons.filesAndFolder.outline.archive;
       default:
-        return Assets.icons.filesAndFolder.solid.file04;
+        return Assets.icons.filesAndFolder.outline.file04;
     }
   }
 }
