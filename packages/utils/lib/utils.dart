3d0249b1c0100af8fa81d307dff51df095051b58
Simon Pham <simonpham.dn@gmail.com>
2023-10-19 00:20:36 +0700

    ➕ Add easy_debounce package

--- packages/utils/lib/utils.dart
+++ packages/utils/lib/utils.dart
@@ -1,5 +1,7 @@
 library utils;
 
+export 'package:easy_debounce/easy_debounce.dart';
+export 'package:easy_debounce/easy_throttle.dart';
 export 'package:fluda/fluda.dart';
 export 'package:mime/mime.dart';
 export 'package:universal_platform/universal_platform.dart';
