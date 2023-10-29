217be2323730f2dee3501e0930bc186b80cf611f
Simon Pham <simonpham.dn@gmail.com>
2023-10-29 17:55:25 +0700

    ♻️ Move SideBarItem to widgets folder

--- apps/ff_desktop/lib/ui/frames/side_bar.dart
+++ apps/ff_desktop/lib/ui/frames/side_bar.dart
@@ -2,6 +2,7 @@ import 'dart:io' as io;
 
 import 'package:core_ui/core_ui.dart';
 import 'package:ff_desktop/constants/constants.dart';
+import 'package:ff_desktop/ui/ui.dart';
 import 'package:ff_desktop/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart';
@@ -152,92 +153,5 @@ class _SideBarSectionState extends State<SideBarSection> {
         ],
       );
     });
-  }
-}
-
-class SideBarItem extends StatelessWidget {
-  final String? title;
-  final Uri? uri;
-  final VoidCallback? onTap;
-
-  final bool selected;
-
-  final SvgGenImage? icon;
-  final SvgGenImage? selectedIcon;
-  final TextStyle? textStyle;
-
-  const SideBarItem({
-    super.key,
-    this.title,
-    this.uri,
-    this.onTap,
-    this.icon,
-    this.selectedIcon,
-    this.textStyle,
-    this.selected = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: Spacing.d32,
-      margin: EdgeInsets.symmetric(
-        horizontal: Spacing.d12,
-      ),
-      child: Tappable(
-        mouseCursor: SystemMouseCursors.click,
-        onTap: onTap,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            color: selected
-                ? context.theme.colorScheme.surface.withTransparency
-                : null,
-            borderRadius: BorderRadius.circular(Spacing.d4 + Spacing.d2),
-          ),
-          child: Row(
-            children: [
-              SizedBox(width: Spacing.d8),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: Spacing.d4,
-                ),
-                child: ImageView(
-                  selected ? selectedIcon : icon,
-                  size: Spacing.d20,
-                  color: selected
-                      ? context.theme.primaryColor
-                      : context.theme.iconTheme.color,
-                ),
-              ),
-              SizedBox(width: Spacing.d8),
-              Expanded(
-                child: Text(
-                  title ?? uri?.lastNonEmptySegment ?? '',
-                  style: (textStyle ?? context.theme.textTheme.bodyLarge)
-                      ?.copyWith(
-                    color: selected ? context.theme.primaryColor : null,
-                    fontWeight: selected ? FontWeight.w700 : null,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              if (selected)
-                Container(
-                  height: Spacing.d32,
-                  width: Spacing.d4 + Spacing.d2,
-                  decoration: BoxDecoration(
-                    color: context.theme.primaryColor,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(Spacing.d4 + Spacing.d2),
-                      bottomRight: Radius.circular(Spacing.d4 + Spacing.d2),
-                    ),
-                  ),
-                ),
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
--- apps/ff_desktop/lib/ui/widgets/widgets.dart
+++ apps/ff_desktop/lib/ui/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'window_title_bar.dart';
 export 'select_rectangle_overlay.dart';
+export 'side_bar_item.dart';
